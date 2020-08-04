f3660109a17038ece0934c9b3d09047e9b4522f7
Nick Bykon <nikita.bikon@gmail.com>
2020-08-04 09:21:32 +0300

    add EJS template engine

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const PORT = process.env.PORT || 3000
 
 const app = express()
 
-app.get('/', (req, res) => res.send("Hellow Thanks that you're working !!!!"))
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => res.render('index'));
 
 app.listen(PORT, () => console.log('Server has been started...')) 
