b7d713ec83e6ff417b6f33e417c416988f3db529
Nick Bykon <nikita.bikon@gmail.com>
2020-08-04 10:20:17 +0300

    add static resources and layout components

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  '/javascripts',
+  express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist'))
+);
 
 app.get('/', (req, res) => { res.render('index'); });
 
