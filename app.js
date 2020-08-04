e7e04c216ff57bc843354e74b0f7e2d9bdbde913
Nick Bykon <nikita.bikon@gmail.com>
2020-08-04 10:59:46 +0300

    add Gulp and stylesheets

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
+const staticAsset = require('static-asset');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+app.use(staticAsset(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   '/javascripts',
--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,1 +1,1 @@
-// scripts
+$((function(){$(".switch-button").on("click",(function(t){t.preventDefault(),$("input").val(""),$(".login").toggle(),$(".register").toggle()}))}));
